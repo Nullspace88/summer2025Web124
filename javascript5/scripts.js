0799218a7f460733fc23e523415bc1d12df5992e
brendan <brendan.oconnor.913@gmail.com>
2025-07-16 19:33:25 -0500

    add javascript5 project

---
+++ javascript5/scripts.js
@@ -0,0 +1,58 @@
+// Brendan O'Connor 7/16/25
+let countdown
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+const audio = document.querySelector('audio');
+const alarm = document.querySelectorAll('audio')[1];
+
+function timer(seconds) {
+    // clear any exisiting timers
+    clearInterval(countdown);
+    
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+    
+    countdown = setInterval(() => {
+	const secondsLeft = Math.round((then - Date.now()) / 1000);
+	audio.play();
+	if(secondsLeft < 0) {
+	    alarm.play();
+	    clearInterval(countdown);
+	    return;
+	}
+	displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+    console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds)
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+});
